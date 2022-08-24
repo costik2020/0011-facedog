66d8b62fc831aa2697c06812131f1670ce56b125
costikluciu2020 <costikluciu2020@gmail.com>
2022-08-24 17:01:57 +0100

    Make a successful API request from within the server

--- server.js
+++ server.js
@@ -14,19 +14,32 @@ So what I want to do is this:
 
 
 const http = require('http');
+const https = require("https");
 const fs = require('fs');
 
+const Stream = require("stream").Transform;
 
 //const hostname = '127.0.0.1';
 // Set the hostname and port to work with heroku
 const hostname = process.env.YOUR_HOST || '0.0.0.0';
 const port = process.env.PORT || 3000;
 
+
+
+
+
+
 fs.readFile('./public/index.html', (err, html)=>{
 	if(err){
 		throw(err);
 	}else{
 		const server = http.createServer((req, res) => {
+
+
+
+
+
+
 		  res.statusCode = 200;
 		  //res.setHeader('Content-Type', 'text/plain');
 		  res.setHeader('Content-Type', 'text/html');
@@ -36,6 +49,31 @@ fs.readFile('./public/index.html', (err, html)=>{
 
 		server.listen(port, hostname, () => {
 		  console.log(`Server running at http://${hostname}:${port}/`);
+
+			  //- Make an api request to the third-pary api. From my server.
+			  https
+				.get("https://dog.ceo/api/breeds/image/random", resp => {
+				  let data = "";
+
+				  // A chunk of data has been recieved.
+				  resp.on("data", chunk => {
+					data += chunk;
+				  });
+
+				  // The whole response has been received. Print out the result.
+				  resp.on("end", () => {
+					let url = JSON.parse(data);
+					console.log("Dog API end point url=",url);
+				  });
+
+				})
+				.on("error", err => {
+				  console.log("Hmm something terible went wrong.");
+				  console.log("Error: " + err.message);
+				});
+	  
+
+
 		});
 	}
 } );
